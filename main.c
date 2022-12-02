782fecf38b56b3c8af221b0640bae818102636cf
lucas.dias <lucas.dias@integrativa.com.br>
2022-12-01 23:18:57 -0300

    refact: Alterado como é a primeira geração de cartas refact: Removida função que gerava as cartas inicias fix: Alterado i++ para i-- nas ultimas jogadas na ordem decrescente fix: Removido -1 da função que mostrava TODAS as jogdas de trás pra frente docs: Adicionado comentários extras as funções e código feat: Agora a carta inicial é puxada automaticamente refact: Nome do jogador agora aparece ao invés de Jogador 1|2 refact: Removido delay duplicado das cartas do computador feat: Adicionado controle quando o rollback era para o início fix: Correção na liberação de memória nas jogadas restantes (não utilizadas)

--- main.c
+++ main.c
@@ -27,13 +27,13 @@ struct j {
     int quantity;           // Quantidade de cartas que o jogador tinha antes de
                             // retirar uma
     char retrievedCard;     // Carta que o jogador retirou
-    struct j *next;         // Ponteiro para a próx jogada
-    struct j *prev;         // Ponteiro para a jogada anterior
+    struct j* next;         // Ponteiro para a próx jogada
+    struct j* prev;         // Ponteiro para a jogada anterior
 } typedef Play;
 
 struct js {
-    Play *first;  // Ponteiro para a primeira jogada
-    Play *last;   // Ponteiro para a última jogada
+    Play* first;  // Ponteiro para a primeira jogada
+    Play* last;   // Ponteiro para a última jogada
     int size;     // Tamanho da lista
 } typedef Plays;
 
@@ -41,47 +41,52 @@ struct js {
  * nomes de jogadores */
 Plays startup(Player players[2]);
 
-// Função que gera 2 cartas aleatórias para cada jogador
-void generateInitialRandomCards(Player players[2], Plays *plays);
-
 // Função para seguir o jogo no modo player vs player
-void pvp(Plays *plays, Player players[2]);
+void pvp(Plays* plays, Player players[2]);
 
 // Função para seguir o jogo no modo player vs computador
-void pve(Plays *plays, Player players[2]);
+void pve(Plays* plays, Player players[2]);
 
 /* Função que retorna as jogadas realizadas, limitadas pela quantidade desejada
  * e com duas direções possíveis, do primeiro em diente, ou do último para trás
  */
-void showPlays(Plays *plays, int quantity, int direction, Player players[2]);
+void showPlays(Plays* plays, int quantity, int direction, Player players[2]);
 
 // Função que mostra a mão de um jogador
 void showHand(Player player);
 
 // Função que atribui uma nova carta para um jogador e registra a jogada
-void getCard(int playerId, Player *player, Plays *plays);
+void getCard(int playerId, Player* player, Plays* plays);
 
 // Função que cria uma nova jogada e a adiciona na lista de jogadas
-void createPlay(Plays *plays, int playerId, Player *player, char retrievedCard);
+void createPlay(Plays* plays, int playerId, Player* player, char retrievedCard);
 
 // Função que verifica e declara empate ou os ganhadores
 // gameType = 1 - player vs player, 2 - player vs computador
-void declareWinner(Plays *plays, Player players[2], int gameType);
+void declareWinner(Plays* plays, Player players[2], int gameType);
 
 // Função que retorna em uma jogada da lista
 // gameType = 1 - player vs player, 2 - player vs computador
-void rollback(Plays *plays, Player players[2], int rollBackTo, int direction,
+void rollback(Plays* plays, Player players[2], int rollBackTo, int direction,
               int gameType);
 
 // Função que reseta os jogadores
 void resetPlayers(Player players[2]);
 
 int main() {
+    // Alocando memória para os jogadores e jogadas
     Player players[2];
-    Plays *plays = malloc(sizeof(Plays));
+    Plays* plays = malloc(sizeof(Plays));
 
     *plays = startup(players);  // Inicializando e preparando terreno
 
+    printf("\n\nDistribuindo cartas");
+    for (int i = 0; i < 3; i++) {
+        printf(".");
+        Sleep(250);
+    }
+
+    // Definindo modo de jogo
     if (players[1].type == 1) {
         pvp(plays, players);
     } else {
@@ -130,41 +135,28 @@ Plays startup(Player players[2]) {
         Sleep(1000);
     }
 
-    printf("\n\nDistribuindo cartas");
-    for (int i = 0; i < 3; i++) {
-        printf(".");
-        Sleep(1000);
-    }
+    // Inicializando as mãos dos jogadores
+    strcpy(players[0].cards, "");
+    strcpy(players[1].cards, "");
 
-    generateInitialRandomCards(players, &plays);
+    // Inicializando as variáveis de controle
+    players[0].quantity = 0;
+    players[1].quantity = 0;
+
+    players[0].stopped = 0;
+    players[1].stopped = 0;
+
+    players[0].points = 0;
+    players[1].points = 0;
 
     system("cls");
 
     return plays;
 }
 
-void generateInitialRandomCards(Player players[2], Plays *plays) {
-    srand(time(NULL));
-    players[0].cards[0] = cards[rand() % 13];
-    players[0].quantity = 1;
-    createPlay(plays, 0, &players[0], players[0].cards[0]);
-
-    players[0].cards[1] = cards[rand() % 13];
-    players[0].quantity = 2;
-    createPlay(plays, 0, &players[0], players[0].cards[1]);
-
-    srand(time(NULL) * 2);
-    players[1].cards[0] = cards[rand() % 13];
-    players[1].quantity = 1;
-    createPlay(plays, 1, &players[1], players[1].cards[0]);
-
-    players[1].cards[1] = cards[rand() % 13];
-    players[1].quantity = 2;
-    createPlay(plays, 1, &players[1], players[1].cards[1]);
-}
-
-void createPlay(Plays *plays, int playerId, Player *player,
+void createPlay(Plays* plays, int playerId, Player* player,
                 char retrievedCard) {
+    // Caso seja a primeira jogada, aloca e cria a primeira jogada
     if (plays->first == NULL) {
         plays->first = malloc(sizeof(Play));
         plays->first->player = playerId;
@@ -188,9 +180,9 @@ void createPlay(Plays *plays, int playerId, Player *player,
     plays->size = plays->size + 1;
 }
 
-void showPlays(Plays *plays, int quantity, int direction, Player players[2]) {
+void showPlays(Plays* plays, int quantity, int direction, Player players[2]) {
     if (direction == 0) {
-        Play *current = plays->first;
+        Play* current = plays->first;
 
         if (quantity != -1) {
             printf("\nMostrando as %d ultimas jogadas em ordem crescente\n",
@@ -227,7 +219,7 @@ void showPlays(Plays *plays, int quantity, int direction, Player players[2]) {
             }
         }
     } else {
-        Play *current = plays->last;
+        Play* current = plays->last;
 
         if (quantity != -1) {
             printf("\nMostrando as %d ultimas jogadas em ordem decrescente\n",
@@ -242,13 +234,13 @@ void showPlays(Plays *plays, int quantity, int direction, Player players[2]) {
                     printf("%c ", current->cards[j]);
                 }
                 printf("\n\n");
-                i++;
+                i--;
 
                 current = current->prev;
             }
         } else {
             printf("\nMostrando TODAS as jogadas em ordem decrescente\n");
-            int i = plays->size - 1;
+            int i = plays->size;
             int j;
             while (current != NULL) {
                 printf("Indice: %d\n%s retirou a carta %c e ficou com: ", i,
@@ -266,7 +258,7 @@ void showPlays(Plays *plays, int quantity, int direction, Player players[2]) {
     }
 }
 
-void getCard(int playerId, Player *player, Plays *plays) {
+void getCard(int playerId, Player* player, Plays* plays) {
     // Gerando carta aleatória e atribuindo ao jogador
     srand(time(NULL));
 
@@ -278,7 +270,8 @@ void getCard(int playerId, Player *player, Plays *plays) {
     createPlay(plays, playerId, player, card);
 }
 
-void checkPoints(Player *player) {
+void checkPoints(Player* player) {
+    // Verificando os valores das cartas e somando os pontos
     int points = 0;
     for (int i = 0; i < player->quantity; i++) {
         if (player->cards[i] == 'A') {
@@ -291,6 +284,8 @@ void checkPoints(Player *player) {
         }
     }
 
+    // Se passou de 21, verifica o valor da carta A (ela vale 11 pontos no
+    // início, mas volta a valer 1 no final)
     if (points > 21) {
         for (int i = 0; i < player->quantity; i++) {
             if (player->cards[i] == 'A') {
@@ -299,6 +294,7 @@ void checkPoints(Player *player) {
         }
     }
 
+    // Se o jogador passou de 21 pontos, ele perde
     if (points > 21) {
         printf("%s estourou com %d pontos\n", player->name, points);
         player->stopped = 1;
@@ -315,8 +311,9 @@ void showHand(Player player) {
     int i;
     printf("\t\n------Cartas %s------\n", player.name);
 
+    // Loop pelas cartas do jogador
     for (i = 0; i < player.quantity; i++) {
-        if (player.cards[i] == 'D') {
+        if (player.cards[i] == 'D') {  // D = 10
             printf(
                 "---\n"
                 "|10|\n"
@@ -336,27 +333,35 @@ void showHand(Player player) {
 
 // Pergunta pra J1 -> Continua? Se não, J1.stopped = 1
 // Pergunta pra J2 -> Continua? Se não, J1.stopped = 1
-void pvp(Plays *plays, Player players[2]) {
+void pvp(Plays* plays, Player players[2]) {
     int opP1, opP2;
     printf("\t\n-----Jogador contra Jogador-----\n\t");
     checkPoints(&players[0]);
 
     while (players[0].stopped != 1) {
-        printf(
-            "\nJogador 1 voce deseja puxar outra carta? (1-Sim; 2-Nao; "
-            "3-Mostrar "
-            "Jogadas)\n>");
-        scanf("%d", &opP1);
+        if (players[0].quantity == 0) {
+            opP1 = 1;
+        } else {
+            printf(
+                "\n%s voce deseja puxar outra carta? (1-Sim; 2-Nao; "
+                "3-Mostrar "
+                "Jogadas)\n>",
+                players[0].name);
+            scanf("%d", &opP1);
+        }
 
         system("cls");
 
         switch (opP1) {
+                // Puxa uma carta
             case 1:
                 getCard(0, &players[0], plays);
                 break;
+                // Para de puxar cartas
             case 2:
                 players[0].stopped = 1;
                 break;
+                // Mostra as jogadas
             case 3:
                 showPlays(plays, -1, 0, players);
                 break;
@@ -371,21 +376,29 @@ void pvp(Plays *plays, Player players[2]) {
     system("cls");
     checkPoints(&players[1]);
     while (players[1].stopped != 1) {
-        printf(
-            "\nJogador 2 voce deseja puxar outra carta? (1-Sim; 2-Nao; "
-            "3-Mostrar "
-            "Jogadas)\n>");
-        scanf("%d", &opP2);
+        if (players[1].quantity == 0) {
+            opP2 = 1;
+        } else {
+            printf(
+                "\n%s voce deseja puxar outra carta? (1-Sim; 2-Nao; "
+                "3-Mostrar "
+                "Jogadas)\n>",
+                players[1].name);
+            scanf("%d", &opP2);
+        }
 
         system("cls");
 
         switch (opP2) {
+            // Puxa uma carta
             case 1:
                 getCard(1, &players[1], plays);
                 break;
+            // Para de puxar cartas
             case 2:
                 players[1].stopped = 1;
                 break;
+            // Mostra as jogadas
             case 3:
                 showPlays(plays, -1, 0, players);
                 break;
@@ -400,26 +413,35 @@ void pvp(Plays *plays, Player players[2]) {
     declareWinner(plays, players, 1);
 }
 
-void pve(Plays *plays, Player players[2]) {
+void pve(Plays* plays, Player players[2]) {
     int op;
     printf("\t\n-----Jogador contra Computador-----\n\t");
     checkPoints(&players[0]);
 
     while (players[0].stopped != 1) {
-        printf(
-            "\nJogador voce deseja puxar outra carta? (1-Sim; 2-Nao; 3-Mostrar "
-            "Jogadas)\n>");
-        scanf("%d", &op);
+        if (players[0].quantity == 0) {
+            op = 1;
+        } else {
+            printf(
+                "\n%s voce deseja puxar outra carta? (1-Sim; 2-Nao; "
+                "3-Mostrar "
+                "Jogadas)\n>",
+                players[0].name);
+            scanf("%d", &op);
+        }
 
         system("cls");
 
         switch (op) {
+                // Puxa uma carta
             case 1:
                 getCard(0, &players[0], plays);
                 break;
+                // Para de puxar cartas
             case 2:
                 players[0].stopped = 1;
                 break;
+                // Mostra as jogadas
             case 3:
                 showPlays(plays, -1, 0, players);
                 break;
@@ -431,10 +453,12 @@ void pve(Plays *plays, Player players[2]) {
         checkPoints(&players[0]);
     }
 
+    // Jogadas do computador (automáticas até ele atingir algum dos fatores de
+    // parada)
     checkPoints(&players[1]);
     while (players[1].stopped != 1) {
         if (players[1].points >= MAX_DEALER_POINTS ||
-            players[1].quantity == MAX_DEALER_CARDS) {
+            players[1].quantity >= MAX_DEALER_CARDS) {
             players[1].stopped = 1;
         } else {
             getCard(1, &players[1], plays);
@@ -442,15 +466,16 @@ void pve(Plays *plays, Player players[2]) {
 
         Sleep(1250);
 
-        checkPoints(&players[1]);
-
-        Sleep(1250);
+        if (players[1].stopped == 0) {
+            checkPoints(&players[1]);
+            Sleep(1250);
+        }
     }
 
     declareWinner(plays, players, 2);
 }
 
-void declareWinner(Plays *plays, Player players[2], int gameType) {
+void declareWinner(Plays* plays, Player players[2], int gameType) {
     system("cls");
 
     printf("\n\nPontos %s: %d", players[0].name, players[0].points);
@@ -512,7 +537,7 @@ void declareWinner(Plays *plays, Player players[2], int gameType) {
         scanf("%d", &direction);
 
         int quantity;
-        printf("\nQuantas jogadas deseja ver? (-1 para todas)\n> ");
+        printf("\nDeseja ver a partir de qual jogada? (-1 para todas)\n> ");
         scanf("%d", &quantity);
 
         showPlays(plays, quantity, direction - 1, players);
@@ -559,42 +584,52 @@ void resetPlayers(Player players[2]) {
     strcpy(players[1].cards, "");
 }
 
-void rollback(Plays *plays, Player players[2], int rollBackTo, int direction,
+void rollback(Plays* plays, Player players[2], int rollBackTo, int direction,
               int gameType) {
     resetPlayers(players);
 
     if (direction == 0) {
-        Play *current = plays->first;
+        Play* current = plays->first;
 
-        // Loopando por todas as jogadas e refazendo a mão do jogador
-        for (int i = 0; i < rollBackTo; i++) {
+        if (rollBackTo <= 0) {
+            // Resetando para o primeiro estado
             if (current->player == 0) {
                 strcpy(players[0].cards, current->cards);
+                strcpy(players[1].cards, "");
             } else {
                 strcpy(players[1].cards, current->cards);
+                strcpy(players[0].cards, "");
             }
+        } else {
+            // Loopando por todas as jogadas e refazendo a mão do jogador
+            for (int i = -1; i < rollBackTo; i++) {
+                if (current->player == 0) {
+                    strcpy(players[0].cards, current->cards);
+                } else {
+                    strcpy(players[1].cards, current->cards);
+                }
 
-            current = current->next;
+                current = current->next;
+            }
         }
 
         checkPoints(&players[0]);
         checkPoints(&players[1]);
 
-        // Definindo a jogada desejada como a última
-        plays->last = NULL;
-        plays->size = rollBackTo + 1;
-
-        // Liberando a memória das jogadas não utilizadas
-        Play *next = current->next;
-        current = current->next;
-        while (next != NULL) {
-            free(current);
-            current = next;
-            next = current->next;
+        Play* last = plays->last;
+        Play* prev = last->prev;
+        while (prev != current) {
+            prev = last->prev;
+            free(last);
+            last = prev;
         }
 
+        // Definindo a jogada desejada como a última
+        plays->last = current;
+        plays->size = rollBackTo + 1;
+
     } else {
-        Play *current = plays->last;
+        Play* current = plays->last;
 
         for (int i = plays->size - 1; i >= rollBackTo; i--) {
             if (current->player == 0) {
@@ -609,18 +644,17 @@ void rollback(Plays *plays, Player players[2], int rollBackTo, int direction,
         checkPoints(&players[0]);
         checkPoints(&players[1]);
 
-        // Definindo a jogada desejada como a última
-        plays->last = NULL;
-        plays->size = rollBackTo + 1;
-
-        // Liberando a memória das jogadas não utilizadas
-        Play *next = current->next;
-        current = current->next;
-        while (next != NULL) {
-            free(current);
-            current = next;
-            next = current->next;
+        Play* last = plays->last;
+        Play* prev = last->prev;
+        while (prev != current) {
+            prev = last->prev;
+            free(last);
+            last = prev;
         }
+
+        // Definindo a jogada desejada como a última
+        plays->last = current;
+        plays->size = rollBackTo + 1;
     }
 
     if (gameType == 1) {
